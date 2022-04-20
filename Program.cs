143d00de903e7d2d364b5d68d7b171d747c0cc9c
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-04-20 17:39:47 -0500

    completed access modifiers lecture

--- Program.cs
+++ Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace AccessModifiers
 {
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1990, 8, 2));
+            Console.WriteLine(person.GetBirthdate());
         }
     }
 }
